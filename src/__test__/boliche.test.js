5f1b817c9eb7a2f436264a44abdf419fc9a636fd
RassielMex <rhcp94.rm@gmail.com>
2023-06-06 21:10:07 -0600

    boliche test throws

--- src/__test__/boliche.test.js
+++ src/__test__/boliche.test.js
@@ -1,15 +1,25 @@
 const Boliche = require("../models/boliche");
+const Throw = require("../models/throw");
 
 describe("Boliche class", () => {
-    const bolicheInstance = new Boliche();
+  const bolicheInstance = new Boliche();
 
+  test("function fill", () => {
+    bolicheInstance.fill();
+    expect(bolicheInstance.throws.length).toBe(10);
+  });
 
-    test("function fill", () => { 
-        bolicheInstance.fill();
-        expect(bolicheInstance.throws.length).toBe(10);
-     });
+  test("function score", () => {
+    expect(typeof bolicheInstance.score()).toBe("number");
+  });
 
-     test("function score", () => { 
-        expect(typeof bolicheInstance.score()).toBe("number");
-      });
+  test("throws to be an Array", () => {
+    expect(bolicheInstance.throws).toBeInstanceOf(Array);
+  });
+
+  test("Each Element of Throws to be an instances of Throw ", () => {
+    bolicheInstance.throws.forEach((t) => {
+      expect(t).toBeInstanceOf(Throw);
+    });
+  });
 });
