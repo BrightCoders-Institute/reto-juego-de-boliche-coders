3e84345fd05b2ff230fe86548e8463123a3a89a7
RassielMex <rhcp94.rm@gmail.com>
2023-06-06 19:52:53 -0600

    throws test Co-Authored-By: Arnoldo Magaña <91897457+AMCernas@users.noreply.github.com> Co-Authored-By: Alfredo Paz (Charibu) <70121105+Alfredo20164920@users.noreply.github.com> Co-authored-by: Montserrat Aguilar <montsegv-2@users.noreply.github.com>

---
+++ __test__/throw.test.js
@@ -0,0 +1,38 @@
+const Throw = require("../throw");
+
+describe("Throw Class", () => {
+  const throwInstance = new Throw();
+
+  test("firstThrowValue is menor o igual que 10", () => {
+    expect(throwInstance.firstThrowValue).toBeLessThanOrEqual(10);
+  });
+
+  test("firstThrowValue is mayor o igual que 0", () => {
+    expect(throwInstance.firstThrowValue).toBeGreaterThanOrEqual(0);
+  });
+
+  test("secondThrowValue is menor o igual que (10 - firstThrowValue)", () => {
+    expect(throwInstance.secondThrowValue).toBeLessThanOrEqual(
+      10 - throwInstance.firstThrowValue
+    );
+  });
+
+  test("secondThrowValue is mayor o igual que 0", () => {
+    expect(throwInstance.secondThrowValue).toBeGreaterThanOrEqual(0);
+  });
+
+  test("thirdThrowValue is mayor o igual que 0", () => {
+    throwInstance.thirdThrow();
+    expect(throwInstance.thirdThrowValue).toBeGreaterThanOrEqual(0);
+  });
+
+  test("totalThrows = firstThrowValue + secondThrowValue", () => {
+    expect(throwInstance.totalThrows).toBe(
+      throwInstance.firstThrowValue + throwInstance.secondThrowValue
+    );
+  });
+
+  test("totalRound is number", () => {
+    expect(typeof throwInstance.totalRound).toBe("number");
+  });
+});
