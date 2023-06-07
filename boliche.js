5dbab08d4e215ac58c294d010d4eb29b65073ba9
RassielMex <rhcp94.rm@gmail.com>
2023-06-06 18:01:49 -0600

    feat total score, fixed strike Co-Authored-By: Arnoldo Magaña <91897457+AMCernas@users.noreply.github.com> Co-Authored-By: Alfredo Paz (Charibu) <70121105+Alfredo20164920@users.noreply.github.com> Co-authored-by: Montserrat Aguilar <montsegv-2@users.noreply.github.com>

--- boliche.js
+++ boliche.js
@@ -14,30 +14,37 @@ class JuegoDeBoliche {
 
     this.throws.forEach((item, index) => {
       if (index < 9) {
-        if (item.totalRound === 10 && item.firstThrowValue === 10 && item.secondThrowValue === 0) {
+        if (
+          //Strike
+          item.firstThrowValue === 10 &&
+          item.secondThrowValue === 0
+        ) {
           if (this.throws[index + 1].totalRound === 10) {
             totalScore += 20 + this.throws[index + 2].firstThrowValue;
           } else {
             totalScore += 10 + this.throws[index + 1].totalThrows;
           }
-        } else if (item.totalThrows === 10){
-            totalScore += 10 + this.throws[index + 1].firstThrowValue;
+        } else if (item.totalThrows === 10) {
+          totalScore += 10 + this.throws[index + 1].firstThrowValue;
         } else {
-            totalScore += item.totalThrows;
+          totalScore += item.totalThrows;
         }
+      } else {
+        if (item.totalRound === 10) {
+          item.thirdThrow = Math.floor(Math.random() * (10 - 0 - 0 + 1) - 0);
+          totalScore +=
+            10 + item.firstThrowValue + item.secondThrowValue + item.thirdThrow;
+        } else if (item.totalThrows === 10) {
+          item.thirdThrow = Math.floor(Math.random() * (10 - 0 - 0 + 1) - 0);
+          totalScore += 10 + item.thirdThrow;
         } else {
-          if (item.totalRound === 10 ) {
-            item.thirdThrow = Math.floor(Math.random() * (10 - 0 - 0 + 1) - 0);
-            totalScore += 10 + item.firstThrowValue + item.secondThrowValue + item.thirdThrow;
-          } else if (item.totalThrows === 10) {
-            item.thirdThrow = Math.floor(Math.random() * (10 - 0 - 0 + 1) - 0);
-            totalScore += 10 + item.thirdThrow;
-          } else {
-            totalScore += item.totalThrows;
-          }    }
+          totalScore += item.totalThrows;
+        }
+      }
+      item.totalRound = totalScore;
     });
-    return totalScore;    
-}
+    return totalScore;
+  }
 }
 
 class Throw {
@@ -64,11 +71,11 @@ game.score();
 
 game.throws.forEach((t, index) => {
   console.log(`Throw ${index + 1}`);
-  console.log (t);
+  console.log(t);
 });
 
 const totalScore = game.score();
-console.log('Total Score:', totalScore);
+console.log("Total Score:", totalScore);
 
 // for(let i = 0; i < 10; i++) {
 //   const exa = new Throw();
